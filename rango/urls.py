c579552d08c35a4dbe0c594ae4acc9be848f44fc
Josh McPhail <2877188M@student.gla.ac.uk>
2025-01-24 00:11:36 +0000

    Chapter 3 complete

---
+++ rango/urls.py
@@ -0,0 +1,11 @@
+#urls.py for rango/
+
+from django.urls import path
+from rango import views
+
+app_name = "rango"
+
+urlpatterns = [
+    path("", views.index, name = "index"),
+    path("about/", views.about, name = "about")
+]
