a6027a8541e54a52a429aa38fb9066b929e3d499
Josh McPhail <2877188M@student.gla.ac.uk>
2025-01-29 09:51:14 +0000

    First data driven page, and slug field

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    #Get a list of all categories currently stored, first 5, ordered by likes in descending order
+    category_list = Category.objects.order_by("-likes")[:5]
+
+    context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake!", "categories": category_list}
     
     return render(request, "rango/index.html", context=context_dict)
 
