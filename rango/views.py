1e42fd787fabbcf1276a0a5e69155bea14ea3338
Josh McPhail <2877188M@student.gla.ac.uk>
2025-01-29 13:55:53 +0000

    Program thinks no categories have any pages, will fix later

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rango.models import Category
+from rango.models import Category, Page
 
 def index(request):
     #Get a list of all categories currently stored, first 5, ordered by likes in descending order
@@ -14,3 +14,18 @@ def about(request):
     context_dict = {"boldmessage": "This tutorial has been put together by Josh McPhail."}
     
     return render(request, "rango/about.html", context = context_dict)
+
+def show_category(request, category_name_slug):
+    try:
+        #try to find a category name slug with the given name
+        category = Category.objects.get(slug = category_name_slug)
+        
+        #Get associated pages
+        pages = Page.objects.filter(category = category)
+
+        context_dict = {"category": category, "page": pages}
+    except Category.DoesNotExist:
+        context_dict = {"category": None, "page": None}
+    
+    #Render and return to client
+    return render(request, "rango/category.html", context = context_dict)
