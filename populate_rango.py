d4b04e27a1486628312555bb41d3e74caa67fec7
Josh McPhail <2877188M@student.gla.ac.uk>
2025-01-29 17:08:42 +0000

    End chapter 6

--- populate_rango.py
+++ populate_rango.py
@@ -7,16 +7,16 @@ django.setup()
 from rango.models import Category, Page
 def populate():
     #Pages info
-    python_pages = [{"title": "Official Python Tutorial", "url": "http://docs.python.org/3/tutorial/"},
-                   {"title": "How to Think like a Computer Scientist", "url": "http://www.greenteapress.com/thinkpython/"},
-                   {"title": "Learn Python in 10 Minutes", "url": "http://www.korokithakis.net/tutorials/python/"}]
+    python_pages = [{"title": "Official Python Tutorial", "url": "http://docs.python.org/3/tutorial/", "views": "20"},
+                   {"title": "How to Think like a Computer Scientist", "url": "http://www.greenteapress.com/thinkpython/", "views": "100"},
+                   {"title": "Learn Python in 10 Minutes", "url": "http://www.korokithakis.net/tutorials/python/", "views": "50"}]
     
-    django_pages = [{"title": "Official Django Tutorial", "url": "https://docs.djangoproject.com/en/2.1/intro/tutorial01/"},
-                   {"title": "Django Rocks", "url": "http://www.djangorocks.com/"},
-                   {"title": "How to Tango with Django", "url": "http://www.tangowithdjango.com/"}]
+    django_pages = [{"title": "Official Django Tutorial", "url": "https://docs.djangoproject.com/en/2.1/intro/tutorial01/", "views": "70"},
+                   {"title": "Django Rocks", "url": "http://www.djangorocks.com/", "views": "20"},
+                   {"title": "How to Tango with Django", "url": "http://www.tangowithdjango.com/", "views": "30"}]
     
-    other_pages = [{"title": "Bottle", "url": "http://bottlepy.org/docs/dev/"},
-                   {"title": "Flask", "url": "http://flask.pocoo.org"}]
+    other_pages = [{"title": "Bottle", "url": "http://bottlepy.org/docs/dev/", "views": "10"},
+                   {"title": "Flask", "url": "http://flask.pocoo.org", "views": "150"}]
     
     cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
             "Django": {"pages": django_pages, "views": 64, "likes": 32},
@@ -27,7 +27,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+            add_page(c, p["title"], p["url"], p["views"])
             
     #Print out categories
     for c in Category.objects.all():
--- rango/views.py
+++ rango/views.py
@@ -5,8 +5,9 @@ from rango.models import Category, Page
 def index(request):
     #Get a list of all categories currently stored, first 5, ordered by likes in descending order
     category_list = Category.objects.order_by("-likes")[:5]
+    page_list = Page.objects.order_by("-views")[:5]
 
-    context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake!", "categories": category_list}
+    context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake!", "categories": category_list, "pages": page_list}
     
     return render(request, "rango/index.html", context=context_dict)
 
@@ -23,9 +24,9 @@ def show_category(request, category_name_slug):
         #Get associated pages
         pages = Page.objects.filter(category = category)
 
-        context_dict = {"category": category, "page": pages}
+        context_dict = {"category": category, "pages": pages}
     except Category.DoesNotExist:
-        context_dict = {"category": None, "page": None}
+        context_dict = {"category": None, "pages": None}
     
     #Render and return to client
     return render(request, "rango/category.html", context = context_dict)
